8ba199cbd32958389732cb94c0662f5e0fa0ae9d
shrutidhungana <shrutidhungana@prixa.net>
2022-08-02 11:57:17 +0545

    Added redux development tools

--- src/Store/configureStore.jsx
+++ src/Store/configureStore.jsx
@@ -8,7 +8,8 @@ const store = () => {
         combineReducers({
             expenses: expensesReducer,
             filters: filtersReducer
-        })
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
     return store;
